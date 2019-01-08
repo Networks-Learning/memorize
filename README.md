06407027e99b3c34287eaab5e1cf626fec193a58
Behzad Tabibian <me@btabibian.com>
2019-01-08 21:21:39 +0100

    Memorize sample algorithm

---
+++ README.md
@@ -0,0 +1,26 @@
+# Memorize
+
+This is a repository containing code for the paper:
+
+> B. Tabibian, U. Upadhyay, A. De, A. Zarezade, Bernhard Schölkopf, and M. Gomez-Rodriguez. Enhancing human learning via spaced repetition. To appear in Proceedings of National Academy of Sciences.
+
+## Pre-requisites
+
+This code depends on the following packages:
+
+ 1. `numpy`: Installation instructions are at [http://www.numpy.org/](http://www.numpy.org/) or `pip install numpy`.
+ 2. `pandas`: Installation instructions are at [https://pandas.pydata.org/](https://pandas.pydata.org/) or `pip install pandas`.
+
+## Code structure
+
+ - `memorize.py` contains the algorithm to obtain samples from optimal reviewing intensity.
+ - `preprocesed_weights.csv` contains weights for [HLR model]() as described in section 8 of supplementary materials.
+ - `observations_1k.csv` contains a set of 1K user-item pairs and associated number of total/correct attempts by every user for given items.
+
+ ## Execution
+
+ The code can by executed as follows:
+
+ `python memorize.py`
+
+ The code will use default parameter value (q) used in the code.
---
+++ memorize.py
@@ -0,0 +1,64 @@
+####
+# This code contains the algorithm to obtain samples from optimal reviewing intensity.
+# This script read two csv files, preprocesed_weights.csv, observations_1k.csv and outputs revieiw times.
+# For user-item pairs that are not reviewed in the period, we show empty string.
+####
+
+import numpy as np
+import pandas as pd
+
+q = 0.1  # parameter q defined in eq. 8 in the paper.
+T = 10.0  # number of days in the future to generate reviewing timeself.
+
+
+def intensity(t, n_t, q):
+    return 1.0/np.sqrt(q)*(1-np.exp(-n_t*t))
+
+
+def sampler(n_t, q, T):
+    t = 0
+    while(True):
+        max_int = 1.0/np.sqrt(q)
+        t_ = np.random.exponential(1 / max_int)
+        if t_ + t > T:
+            return None
+        t = t+t_
+        proposed_int = intensity(t, n_t, q)
+        if np.random.uniform(0, 1, 1)[0] < proposed_int / max_int:
+            return t
+
+if __name__ == '__main__':
+    output = []
+    output_file = open("output.csv", "w")
+    results_hlr = pd.read_csv("hlr.duolingo.weights",index_col=0)
+
+    results_hlr = results_hlr.set_index("label")
+    right = results_hlr['value'].loc["right"]
+    wrong = results_hlr['value'].loc["wrong"]
+    results_hlr["n0"] = 2**(-(results_hlr['value'][3:]+results_hlr['value'].loc['bias']))
+    duo_alpha = (-2**(-results_hlr['value'].loc['right'])+1)
+    duo_beta = (2**(-results_hlr['value'].loc['wrong'])-1)
+    #results_hlr['lexeme_id'] = results_hlr['lexeme_id'].str.slice(start=3
+    hlr = results_hlr[3:].reset_index().set_index('lexeme_id')
+    i = 0
+    print("Generating reviewing times for user-item pairs")
+    print("Maximum duration: {}, q: {}".format(T, q))
+    with open("observation_1k.csv") as f:
+        f.readline()
+        output_file.write(", ".join(("user-id", "lexeme_id", "review time (in days)\n")))
+        i += 1
+        for line in f:
+            values = line.split(",")
+            lid = values[2]
+            n_correct = float(values[3])
+            n_wrong = float(values[4]) - float(values[3])
+
+            n_t = hlr['value'].loc[lid] *\
+                            2**(-(right*n_correct+\
+                                  wrong*n_wrong))
+            t_rev = sampler(n_t, 1.0, 10.0)
+            output_file.write(", ".join((values[1], values[2], str(t_rev) if t_rev is not None else ""))+"\n")
+            if i%100 == 0:
+                print("Finished processing {} lines.".format(i))
+            i+=1
+    print("Finished generating review times.")
